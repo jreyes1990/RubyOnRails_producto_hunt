e9f780d17cddca325d6722a9b2510ca72d64240f
jreyes1990 <jarl1990mt@gmail.com>
2022-07-04 22:08:11 -0600

    Correccion de error de imagen 04/07/2022 22:08 PM

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   def index
     current_page = params[:page] ||= 1
-    @products = Product.where(visible: true).order('id DESC').paginate(page: current_page,per_page: 2)
+    @products = Product.where(visible: true).order('id DESC').paginate(page: current_page,per_page: 10)
   end
 
   def show
