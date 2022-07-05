3c334724c7f1e90be59817bac24796576a275f46
jreyes1990 <jarl1990mt@gmail.com>
2022-07-04 23:49:11 -0600

    Creacion partial (_form) para crear comentarios 04/07/2022 23:49 PM

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  before_action :set_product, only: [:create]
+
+  def create
+    @product.comments.create(comment_params)
+    #@comment = Comment.new(comment_params)
+    #@comment.product = @product
+    #@comment.save
+
+    redirect_to @product, notice: "Comentario creado de forma exitosa."
+  end
+
+  private
+
+  def set_product
+    @product = Product.find_by slug:params[:product_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
